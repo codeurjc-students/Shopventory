11a66067405e59991898e16b1dd7818916921316
Alberto <aj.garciag.2019@alumnos.urjc.es>
2025-04-27 12:45:09 +0200

    First commit
    Initial commit. Created Maven project and folder structure.

---
+++ backend/shopventory/src/main/java/es/codeurjc/shopventory/shopventory/ShopventoryApplication.java
@@ -0,0 +1,13 @@
+package es.codeurjc.shopventory.shopventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopventoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopventoryApplication.class, args);
+	}
+
+}
---
+++ backend/shopventory/src/test/java/es/codeurjc/shopventory/shopventory/ShopventoryApplicationTests.java
@@ -0,0 +1,13 @@
+package es.codeurjc.shopventory.shopventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopventoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
